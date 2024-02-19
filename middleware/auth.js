6ae81ea202ff1aeb3434c80c69c1440e1e21ef22
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-19 19:39:08 +0530

    bypass auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,22 @@ const jwt = require("jsonwebtoken")
 const config = process.env
 
 const verifyToken = (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.cookies.jwt
+    // const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.cookies.jwt
 
-    if (!token) {
-        return res.status(403).send("Session Expired Login again to continue")
-    }
-    try {
-        const decoded = jwt.verify(token, config.TOKEN_KEY)
-        req.user = decoded
-    } catch (err) {
-        return res.status(401).send("Error occured login again")
-    }
+    // if (!token) {
+    //     return res.status(403).send("Session Expired Login again to continue")
+    // }
+    // try {
+    //     const decoded = jwt.verify(token, config.TOKEN_KEY)
+    //     req.user = decoded
+    // } catch (err) {
+    //     return res.status(401).send("Error occured login again")
+    // }
+    
+    
     return next()
+    
+    // I am unable to resolve the cookies error its getting generated but as soon as i refresh it gets deleted so i had to bypass the auth
 }
 
 module.exports = verifyToken
