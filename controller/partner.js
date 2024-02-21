88442410f97a69a90c4ad98d61269928a596a2e9
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-22 01:44:09 +0530

    samesite corsss

--- controller/partner.js
+++ controller/partner.js
@@ -110,13 +110,13 @@ app.post("/login", async (req, res) => {
             httpOnly: false,
             maxAge: expiresIn === '7d' ? 7 * 24 * 60 * 60 * 1000 : 2 * 60 * 60 * 1000,
             // secure: false,
-            // secure: true,
+            secure: true,
             sameSite: 'None',
         }).cookie('refid', refid, {
             maxAge: expiresIn === '7d' ? 7 * 24 * 60 * 60 * 1000 : 2 * 60 * 60 * 1000,
             httpOnly: false,
             // secure: false,
-            // secure: true,
+            secure: true,
             sameSite: 'None',
         }).json({
             msg: 'Login successful',
